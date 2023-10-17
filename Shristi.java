472f2250e8b4676841b4353272a62e81969b5c59
shristisharma01 <srashtis131@gmail.com>
2023-10-17 21:30:40 +0530

    second commit

--- Shristi.java
+++ Shristi.java
@@ -1,11 +1,5 @@
  public class HelloWorld{
      public static void main (string args[]){
              system.out.println("Hello World!");
-             echo "# JavaFile" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shristisharma01/JavaFile.git
-git push -u origin main
+     }
  }
